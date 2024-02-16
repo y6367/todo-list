221c4a7c464eb894006f95e0cd7afef2b81a1689
y6367 <yjoe2035@gmail.com>
2024-02-16 12:27:45 -0800

    s

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import generateJoke from "./generateJoke";
-import './styles/main.scss'
-import laughing from './assets/laughing.svg'
-
-const laughImg = document.querySelector('#laughImg')
-laughImg.src = laughing
-
-console.log(generateJoke())
